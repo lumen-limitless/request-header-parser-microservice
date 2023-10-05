ffe03359fcd5c0d2da9be1a3f2930d777df5a768
lumen-limitless <lumenlimitless@gmail.com>
2023-10-04 23:39:29 -0400

    allow custom port

--- src/main.rs
+++ src/main.rs
@@ -4,19 +4,23 @@ use hyper::{
     HeaderMap,
 };
 use serde::{Deserialize, Serialize};
-use std::net::SocketAddr;
+use std::{net::SocketAddr, str::FromStr};
 
 #[tokio::main]
 async fn main() {
     // initialize tracing
     tracing_subscriber::fmt::init();
 
+    // read the port from env or use the port default port(8080)
+    let port = std::env::var("PORT").unwrap_or(String::from("8080"));
+    // convert the port to a socket address
+    let addr = SocketAddr::from_str(&format!("0.0.0.0:{}", port)).unwrap();
+
     // build our application with a route
     let app = Router::new().route("/api/whoami", get(who_am_i));
 
     // run our app with hyper
     // `axum::Server` is a re-export of `hyper::Server`
-    let addr = SocketAddr::from(([127, 0, 0, 1], 8200));
     tracing::debug!("listening on {}", addr);
     axum::Server::bind(&addr)
         .serve(app.into_make_service_with_connect_info::<SocketAddr>())
