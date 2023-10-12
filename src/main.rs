c00e1f35b42d300783cd3e956cadd8d1ccc6be70
lumen-limitless <lumenlimitless@gmail.com>
2023-10-12 19:42:02 -0400

    heroku deploy action

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,12 @@
 use axum::{extract::ConnectInfo, routing::get, Json, Router};
 use hyper::{
     header::{ACCEPT_LANGUAGE, USER_AGENT},
-    HeaderMap,
+    HeaderMap, Method,
 };
 use serde::{Deserialize, Serialize};
 use std::{net::SocketAddr, str::FromStr};
-
-#[tokio::main]
-async fn main() {
-    // initialize tracing
-    tracing_subscriber::fmt::init();
-
-    // read the port from env or use the port default port(8080)
-    let port = std::env::var("PORT").unwrap_or(String::from("8080"));
-    // convert the port to a socket address
-    let addr = SocketAddr::from_str(&format!("0.0.0.0:{}", port)).unwrap();
-
-    // build our application with a route
-    let app = Router::new().route("/api/whoami", get(who_am_i));
-
-    // run our app with hyper
-    // `axum::Server` is a re-export of `hyper::Server`
-    tracing::debug!("listening on {}", addr);
-    axum::Server::bind(&addr)
-        .serve(app.into_make_service_with_connect_info::<SocketAddr>())
-        .await
-        .expect("server failed");
-}
+use tower_http::cors::{Any, CorsLayer};
+use tracing::info;
 
 #[derive(Deserialize, Serialize)]
 struct Response {
@@ -56,4 +36,31 @@ async fn who_am_i(
         language,
         software,
     })
+}
+
+#[tokio::main]
+async fn main() {
+    tracing_subscriber::fmt::init();
+
+    let cors = CorsLayer::new()
+        .allow_methods(vec![Method::GET])
+        .allow_origin(Any);
+
+    // read the port from env or use the port default port(8080)
+    let port = std::env::var("PORT").unwrap_or(String::from("8080"));
+    // convert the port to a socket address
+    let addr = SocketAddr::from_str(&format!("0.0.0.0:{}", port)).unwrap();
+
+    // build our application with a route
+    let app = Router::new()
+        .route("/api/whoami", get(who_am_i))
+        .layer(cors);
+
+    // run our app with hyper
+    // `axum::Server` is a re-export of `hyper::Server`
+    info!("listening on {}", addr);
+    axum::Server::bind(&addr)
+        .serve(app.into_make_service_with_connect_info::<SocketAddr>())
+        .await
+        .expect("server failed");
 }
